6e9639cdfeb7580b3f2d7a71bebccbcd447d61d7
caitleigh <caitleigh27@gmail.com>
2020-03-31 16:35:14 -0700

    cart now renders with items added and on the right side of the page next to products

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,13 @@ import './App.scss';
 import Header from '../Header'
 import Categories from '../Categories';
 import Products from '../Products'
+import SimpleCart from '../SimpleCart'
+import { Row, Col } from 'react-bootstrap'
+
 import { Container } from 'react-bootstrap';
 import { Provider } from 'react-redux'
 import store from '../../store'
+
 
 function App() {
 
@@ -13,9 +17,22 @@ function App() {
 
     <Provider store={store}>
       <Container className="App">
-        <Header />
-        <Categories />
-        <Products />
+        <Row>
+          <Header />
+        </Row>
+
+        <Row>
+          <Categories />
+        </Row>
+
+        <Row>
+          <Col>
+            <Products />
+          </Col>
+          <Col>
+            <SimpleCart />
+          </Col>
+        </Row>
       </Container>
     </Provider>
   )
